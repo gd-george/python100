54c6fe4ad5b9697ac6ded766a8b5d3bf57ade552
gd-george <gd.george.gd@gmail.com>
2026-01-27 23:53:24 +0530

    day 8 encode decode

--- Day 8/function.py
+++ Day 8/function.py
@@ -3,3 +3,22 @@ def greet():
 
 
 greet()
+
+#function that allows input
+
+def greet_with_name(name):
+    print(f"Hello, {name}! Welcome to Day 8!")  
+
+
+greet_with_name("George")
+
+#function with more than 1 input
+
+def greet_with(name, location):
+    print(f"Hello, {name}! Welcome to {location}!")
+
+greet_with("George","Port Blair")
+
+#positional argument
+
+greet_with(location="Port Blair", name="George")
